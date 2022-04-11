6d6aa3dd17cc473d0e88824522c74c1a159cfa76
PETEROKENO <43709466+PETEROKENO@users.noreply.github.com>
2022-04-11 17:43:12 +0300

    Initial Commit

---
+++ DrunkManRace.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int main(){
+	
+	srand(time(0));
+    int size = 60;
+    int position = size/2;
+    int sleep = 0;
+    char x;
+	
+	cout<<"Please Enter a letter between a-z"<<endl;
+    cin>>x;
+
+    while(position>1 && position <size && sleep < 100000){
+        cout<<"Start";
+           for(int i =0;i<size;i++){
+               if(i ==position){
+                   cout<<x;
+               }else{
+                   cout<<" ";
+               }
+           }
+        cout<<"Finish"<<endl;
+        int move = rand()%3 -1;
+        position = position + move;
+        sleep++;   
+    }
+    
+    if(position<=1){
+        cout<<"You couldn't finish the race.";
+    }
+    if(position>=size){
+        cout<<"Yay! Congratulations. You finished the race on time.";
+    }
+    if(position != 1 && position != size){
+		cout<<"Time is up. Please try again"<<endl;
+	}
+    return 0;
+}
